22aca0cd2e5940f74e6eebd2b26837b968a34acc
Sankalp <sankalp13353@gmail.com>
2026-01-06 23:02:00 +0530

    appointment till done

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -107,16 +107,26 @@ useEffect(()=>{
         <p>
           Booking Slots
         </p>
-        <div>
+        <div className='flex gap-3 items-center w-full overflow-x-scroll mt-4'>
           {
             docSlots.length && docSlots.map((item,index)=>(
-              <div key={index}>
+              <div onClick={()=>setSlotIndex(index)} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-primary text-white' : 'border border-gray-200 '}`} key={index}>
                 <p>{item[0] && daysOfWeek[item[0].dateTime.getDay()]}</p>
-                <p>{item[0] && item[0].dateTime.getDate( )}</p>
+                <p>{item[0] && item[0].dateTime.getDate()}</p>
               </div>
             ))
           }
         </div>
+
+        <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
+          {docSlots.length && docSlots[slotIndex].map((item,index)=>(
+            <p onClick={()=>setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300'}`} key={index}>
+              {item.time.toLowerCase()}
+            </p>
+          ))}
+        </div>
+
+        <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6 '>Book an appointment</button>
       </div>
 
     </div>
