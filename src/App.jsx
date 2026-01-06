baad4020f4ee2138aa397bcc48914287bfba003c
Sankalp <sankalp13353@gmail.com>
2026-01-06 13:40:03 +0530

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import MyAppointments from "./pages/myAppointments";
 import MyProfile from "./pages/myProfile";
 import Appointment from "./pages/Appointment";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -25,6 +26,7 @@ const App = () => {
         <Route path="/myProfile" element={<MyProfile />} />
         <Route path="/appointment/:docId" element={<Appointment />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
