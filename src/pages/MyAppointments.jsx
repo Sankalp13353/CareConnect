2232d982452e20fc828a4fe502d9c6009d43003e
Sankalp <sankalp13353@gmail.com>
2026-01-10 17:13:07 +0530

    appointment page without css

--- src/pages/MyAppointments.jsx
+++ src/pages/MyAppointments.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext'; 
 
 const MyAppointments = () => {
+
+  const { doctors } = useContext(AppContext);
+
   return (
     <div>
-      
+      <p>My appointments</p>
+      <div>
+        {
+          doctors.slice(0,2).map((item,index)=>(
+            <div key={index}>
+              <div>
+                <img src={item.image} alt="" />
+              </div>
+              <div>
+                <p>{item.name}</p>
+                <p>{item.speciality}</p>
+                <p>Address:</p>
+                <p>{item.address.line1}</p>
+                <p>{item.address.line2}</p>
+                <p><span>Date & Time:</span>1, Jan, 2025 | 9:00 PM</p>
+              </div>
+
+              <div>
+
+              </div>
+              <div>
+                <button>Pay Online</button>
+                <button>Cancel Appointment</button>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
