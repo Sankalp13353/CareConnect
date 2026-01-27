51a02fc40e9924ee36dbe96029d955cb65c1366d
Sankalp <sankalp13353@gmail.com>
2026-01-27 13:49:03 +0530

    add doctor page on admin portal

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,10 +1,107 @@
 import React from 'react';
+import { assets } from '../../assets/assets';
 
 const AddDoctor = () => {
   return (
-    <div>
-      AddDoctor
-    </div>
+    <form >
+
+      <p>Add Doctor</p>
+      
+      <div>
+        <div>
+          <label htmlFor="doc-img">
+            <img src={assets.upload_area} alt="" />
+          </label>
+          <input type="file" id='doc-img' hidden/>
+          <p>Upload doctor <br/> picture</p>
+        </div>
+
+        <div>
+          <div>
+
+
+            <div>
+              <p>Doctor Name</p>
+              <input type="text" placeholder='Name' required/> 
+            </div>
+
+            <div>
+              <p>Doctor Email</p>
+              <input type="email" placeholder='Email ' required/> 
+            </div> 
+
+            <div>
+              <p>Doctor Password</p>
+              <input type="password" placeholder='Password' required/> 
+            </div>
+
+            <div>
+              <p>Experience</p>
+              <select name="" id="">
+                <option value="1 Year">1 Year</option>
+                <option value="2 Years">2 Year</option>
+                <option value="3 Years">3 Year</option>
+                <option value="4 Years">4 Year</option>
+                <option value="5 Years">5 Year</option>
+                <option value="6 Years">6 Year</option>
+                <option value="7 Years">7 Year</option>
+                <option value="8 Years">8 Year</option>
+                <option value="9 Years">9 Year</option>
+                <option value="10 Years">10 Year </option>
+              </select>
+            </div>
+
+            <div>
+              <p>Fees</p>
+              <input type="number" placeholder='Fees' required/> 
+            </div>
+
+
+          </div>
+
+
+          <div>
+
+            <div>
+              <p>Speciality</p>
+              <select name="" id="">
+                <option value="General physician">General physician</option>
+                <option value="Gynecologist">Gynecologist</option>
+                <option value="Dermatologist">Dermatologist</option>
+                <option value="Pediatricians">Pediatricians</option>
+                <option value="Neurologist">Neurologist</option>
+                <option value="Gastroenterologist">Gastroenterologist</option>
+              </select>
+            </div>
+
+
+            <div>
+              <p>Education</p>
+              <input type="text  " placeholder='Education' required/> 
+            </div>
+
+            <div>
+              <p>Address</p>
+              <input type="text" placeholder='address 1' required />
+              <input type="text" placeholder='address 2' required />
+            </div>
+  
+
+          </div>
+        </div>
+
+        <div>
+          <p>About Doctor</p>
+          <textarea placeholder='Write about doctor' rows={5} required/> 
+        </div>
+
+        <button>Add Doctor</button>
+
+         
+
+
+      </div>
+    </form>
   );
 }
 
