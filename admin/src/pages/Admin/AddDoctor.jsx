e681ab750ebf4b0ef175367a2dec3a285fa20270
Sankalp <sankalp13353@gmail.com>
2026-01-27 13:57:06 +0530

    fff

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,38 +1,37 @@
-import React from 'react';
-import { assets } from '../../assets/assets';
+import React from "react";
+import { assets } from "../../assets/assets";
 
 const AddDoctor = () => {
   return (
-    <form >
+    <form className="m-5 w-full">
+      <p className="mb-3 text-lg font-medium ">Add Doctor</p>
 
-      <p>Add Doctor</p>
-      
-      <div>
-        <div>
+      <div className="bg-white px-8 py-8 border rounded w-full max-w-4xl max-h-[80vh] overflow-y-scroll">
+        <div className="flex items-center gap-4 mb-8 text-gray-500">
           <label htmlFor="doc-img">
-            <img src={assets.upload_area} alt="" />
+            <img className="w-16 bg-gray-100 rounded-full cursor-pointer " src={assets.upload_area} alt="" />
           </label>
-          <input type="file" id='doc-img' hidden/>
-          <p>Upload doctor <br/> picture</p>
+          <input type="file" id="doc-img" hidden />
+          <p>
+            Upload doctor <br /> picture
+          </p>
         </div>
 
-        <div>
+        <div className="flex flex-col lg:flex-row items-start gap-10 text-gray-600">
           <div>
-
-
             <div>
               <p>Doctor Name</p>
-              <input type="text" placeholder='Name' required/> 
+              <input type="text" placeholder="Name" required />
             </div>
 
             <div>
               <p>Doctor Email</p>
-              <input type="email" placeholder='Email ' required/> 
-            </div> 
+              <input type="email" placeholder="Email " required />
+            </div>
 
             <div>
               <p>Doctor Password</p>
-              <input type="password" placeholder='Password' required/> 
+              <input type="password" placeholder="Password" required />
             </div>
 
             <div>
@@ -53,15 +52,11 @@ const AddDoctor = () => {
 
             <div>
               <p>Fees</p>
-              <input type="number" placeholder='Fees' required/> 
+              <input type="number" placeholder="Fees" required />
             </div>
-
-
           </div>
 
-
           <div>
-
             <div>
               <p>Speciality</p>
               <select name="" id="">
@@ -74,35 +69,28 @@ const AddDoctor = () => {
               </select>
             </div>
 
-
             <div>
               <p>Education</p>
-              <input type="text  " placeholder='Education' required/> 
+              <input type="text  " placeholder="Education" required />
             </div>
 
             <div>
               <p>Address</p>
-              <input type="text" placeholder='address 1' required />
-              <input type="text" placeholder='address 2' required />
-            </div>
-  
-
+              <input type="text" placeholder="address 1" required />
+              <input type="text" placeholder="address 2" required />
+            </div> 
           </div>
         </div>
 
         <div>
           <p>About Doctor</p>
-          <textarea placeholder='Write about doctor' rows={5} required/> 
+          <textarea placeholder="Write about doctor" rows={5} required />
         </div>
 
         <button>Add Doctor</button>
-
-         
-
-
       </div>
     </form>
   );
-}
+};
 
 export default AddDoctor;
