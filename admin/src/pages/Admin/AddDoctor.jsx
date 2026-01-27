e70c93d0c25f5b5b00a2e9cd3220e239fef24121
Sankalp <sankalp13353@gmail.com>
2026-01-27 16:52:43 +0530

    complete UI for add doctor page

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -18,25 +18,25 @@ const AddDoctor = () => {
         </div>
 
         <div className="flex flex-col lg:flex-row items-start gap-10 text-gray-600">
-          <div>
-            <div>
+          <div className="w-full lg:flex-1 flex flex-col gap-4">
+            <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Name</p>
-              <input type="text" placeholder="Name" required />
+              <input className="border rounded px-3 py-2" type="text" placeholder="Name" required />
             </div>
 
-            <div>
+            <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Email</p>
-              <input type="email" placeholder="Email " required />
+              <input className="border rounded px-3 py-2" type="email" placeholder="Email " required />
             </div>
 
-            <div>
+            <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Password</p>
-              <input type="password" placeholder="Password" required />
+              <input className="border rounded px-3 py-2" type="password" placeholder="Password" required />
             </div>
 
-            <div>
+            <div className="flex-1 flex flex-col gap-1">
               <p>Experience</p>
-              <select name="" id="">
+              <select className="border rounded px-3 py-2" name="" id="">
                 <option value="1 Year">1 Year</option>
                 <option value="2 Years">2 Year</option>
                 <option value="3 Years">3 Year</option>
@@ -50,16 +50,16 @@ const AddDoctor = () => {
               </select>
             </div>
 
-            <div>
+            <div className="flex-1 flex flex-col gap-1">
               <p>Fees</p>
-              <input type="number" placeholder="Fees" required />
+              <input className="border rounded px-3 py-2" type="number" placeholder="Fees" required />
             </div>
           </div>
 
-          <div>
-            <div>
+          <div className="w-full lg:flex-1 flex flex-col gap-4">
+            <div className="flex-1 flex flex-col gap-1">
               <p>Speciality</p>
-              <select name="" id="">
+              <select className="border rounded px-3 py-2" name="" id="">
                 <option value="General physician">General physician</option>
                 <option value="Gynecologist">Gynecologist</option>
                 <option value="Dermatologist">Dermatologist</option>
@@ -69,25 +69,25 @@ const AddDoctor = () => {
               </select>
             </div>
 
-            <div>
+            <div className="flex-1 flex flex-col gap-1">
               <p>Education</p>
-              <input type="text  " placeholder="Education" required />
+              <input className="border rounded px-3 py-2" type="text  " placeholder="Education" required />
             </div>
 
-            <div>
+            <div className="flex-1 flex flex-col gap-1">
               <p>Address</p>
-              <input type="text" placeholder="address 1" required />
-              <input type="text" placeholder="address 2" required />
+              <input className="border rounded px-3 py-2" type="text" placeholder="address 1" required />
+              <input className="border rounded px-3 py-2" type="text" placeholder="address 2" required />
             </div> 
           </div>
         </div>
 
         <div>
-          <p>About Doctor</p>
-          <textarea placeholder="Write about doctor" rows={5} required />
+          <p className="mt-4 mb-2 ">About Doctor</p>
+          <textarea className="w-full px-4 pt-2 border rounded " placeholder="Write about doctor" rows={5} required />
         </div>
 
-        <button>Add Doctor</button>
+        <button className="bg-primary px-10 py-3 mt-4 text-white rounded-full">Add Doctor</button>
       </div>
     </form>
   );
